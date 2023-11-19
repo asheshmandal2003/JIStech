185f5dd0b33e4c8e7086b440a451d0e5403a6d6a
Devayan <devayan9689@gmail.com>
2023-11-19 22:49:40 +0530

    modified

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,21 +1,51 @@
+import { useState, useEffect } from 'react';
 import { Navbar, Nav, Container, Button } from 'react-bootstrap';
 
-const NavigationBar = () => {
-    const isLoggedIn = false;
+function NavigationBar() {
+    const [navbar, setNavbar] = useState(false);
+    const [textColor, setTextColor] = useState('black');
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const changeBackground = () => {
+        if (window.scrollY >= 80) {
+            setNavbar(true);
+            setTextColor('white');
+        } else {
+            setNavbar(false);
+            setTextColor('black');
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', changeBackground);
+        const checkLoginStatus = async () => {
+            const loggedIn = await isLoggedIn();
+            setIsLoggedIn(loggedIn);
+        };
+        checkLoginStatus();
+        return () => {
+            window.removeEventListener('scroll', changeBackground);
+        };
+    }, []);
 
     return (
-        <Navbar bg="dark" variant="dark" expand="lg" className="py-3">
+        <Navbar style={{ backgroundColor: navbar ? "#343a40" : "white", color: textColor }} variant="dark" expand="lg" className="py-3 fixed-top">
             <Container>
-                <Navbar.Brand href="/" className="font-weight-bold text-uppercase">Project X</Navbar.Brand>
+                <Navbar.Brand href="/" style={{ color: textColor }} className="font-weight-bold text-uppercase">Project X</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                        <Nav.Link href="/about" className="mx-3">About</Nav.Link>
-                        <Nav.Link href="/contact" className="mx-3">Contact</Nav.Link>
+                        <Nav.Link href="/about" style={{ color: textColor }} className="mx-3">About</Nav.Link>
+                        <Nav.Link href="/services" style={{ color: textColor }} className="mx-3">Services</Nav.Link>
+                        <Nav.Link href="/community" style={{ color: textColor }} className="mx-3">Community</Nav.Link>
+                        <Nav.Link href="/contact" style={{ color: textColor }} className="mx-3">Contact</Nav.Link>
                         {isLoggedIn ? (
-                            <Nav.Link href="/profile" className="mx-3">Profile</Nav.Link>
+                            <>
+                                <Nav.Link href="/profile" style={{ color: textColor }} className="mx-3">Profile</Nav.Link>
+                                <Nav.Link href="/emergency" style={{ color: textColor }} className="mx-3">Emergency</Nav.Link>
+                            </>
                         ) : (
-                            <Button href="/account" variant="outline-light" className="mx-3">
+                                <Button href="/account" variant="outline-dark" className="mx-3" style={{ color: navbar ? 'black' : 'white', backgroundColor: navbar ? 'white' : '#343a40' }}>
                                 Sign In / Sign Up
                             </Button>
                         )}
@@ -24,6 +54,6 @@ const NavigationBar = () => {
             </Container>
         </Navbar>
     );
-};
+}
 
 export default NavigationBar;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,41 +1,43 @@
-import { Container, Row, Col, Button, Card } from 'react-bootstrap';
+import { Row, Col, Card, Button, Container } from 'react-bootstrap';
 
 const Home = () => {
     return (
-        <Container fluid className="p-0">
-            <div className="hero-section bg-light text-center py-5">
-                <h1 className="display-4">Welcome to Our Application</h1>
-                <p className="lead">Providing safety and security with real-time geolocation services and an SOS function for emergencies.</p>
-            </div>
+        <div>
+            <Container fluid className="p-5 mt-5 bg-dark text-white text-center">
+                <h1>Welcome to Project X</h1>
+                <p>Empowering communities through safety and awareness.</p>
+            </Container>
 
-            <Row className="feature-cards py-5">
-                <Col md={4}>
-                    <Card className="h-100">
-                        <Card.Body>
-                            <Card.Title>Real-time Geolocation</Card.Title>
-                            <Card.Text>Track your location in real-time for enhanced safety.</Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col md={4}>
-                    <Card className="h-100">
-                        <Card.Body>
-                            <Card.Title>SOS Function</Card.Title>
-                            <Card.Text>Send an SOS signal in case of emergencies.</Card.Text>
-                            <Button variant="danger" onClick={() => alert('SOS button clicked!')}>SOS</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col md={4}>
-                    <Card className="h-100">
-                        <Card.Body>
-                            <Card.Title>Community Empowerment</Card.Title>
-                            <Card.Text>Share information with the community for collective safety.</Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
+            <Container className="py-5">
+                <Row className="feature-cards">
+                    <Col md={4}>
+                        <Card className="h-100 shadow">
+                            <Card.Body>
+                                <Card.Title>Real-time Geolocation</Card.Title>
+                                <Card.Text>Track your location in real-time for enhanced safety.</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={4}>
+                        <Card className="h-100 shadow">
+                            <Card.Body>
+                                <Card.Title>SOS Function</Card.Title>
+                                <Card.Text>Send an SOS signal in case of emergencies.</Card.Text>
+                                <Button variant="danger" onClick={() => alert('SOS button clicked!')}>SOS</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={4}>
+                        <Card className="h-100 shadow">
+                            <Card.Body>
+                                <Card.Title>Community Empowerment</Card.Title>
+                                <Card.Text>Join a community of people dedicated to safety and awareness.</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 };
 
