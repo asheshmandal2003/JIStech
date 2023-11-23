bb6fcbd2d5721dc60269c2ce09c0ebf9917ee898
Devayan <devayan9689@gmail.com>
2023-11-23 22:14:39 +0530

    modified

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,98 +3,107 @@ import { Navbar, Nav, Container, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 function NavigationBar({ user }) {
-  const navigate = useNavigate();
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const navigate = useNavigate();
+    // const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  // useEffect(() => {
-  //   const checkLoginStatus = async () => {
-  //     const loggedIn = await isLoggedIn();
-  //     setIsLoggedIn(loggedIn);
-  //   };
-  //   checkLoginStatus();
-  // }, []);
+    // useEffect(() => {
+    //   const checkLoginStatus = async () => {
+    //     const loggedIn = await isLoggedIn();
+    //     setIsLoggedIn(loggedIn);
+    //   };
+    //   checkLoginStatus();
+    // }, []);
 
-  return (
-    <Navbar expand="lg" className="py-3 fixed-top">
-      <Container>
-        <Navbar.Brand
-          href="/"
-          className="font-weight-bold text-uppercase"
-          style={{ color: "#D73CBE" }}
-        >
-          Shield
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link
-              href="/about"
-              className="mx-3"
-              style={{ color: "#D73CBE" }}
-            >
-              About
-            </Nav.Link>
-            <Nav.Link
-              href="/services"
-              className="mx-3"
-              style={{ color: "#D73CBE" }}
-            >
-              Services
-            </Nav.Link>
-            <Nav.Link
-              href="/community"
-              className="mx-3"
-              style={{ color: "#D73CBE" }}
-            >
-              Community
-            </Nav.Link>
-            <Nav.Link
-              href="/contact"
-              className="mx-3"
-              style={{ color: "#D73CBE" }}
-            >
-              Contact
-            </Nav.Link>
-            {user !== null ? (
-              <>
-                <Nav.Link
-                  href="/profile"
-                  className="mx-3"
-                  style={{ color: "#D73CBE" }}
+    return (
+        <Navbar expand="lg" className="py-3 fixed-top" style={{ boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)' }}>
+            <Container>
+                <Navbar.Brand href="/">
+                    <img
+                        src="/logo.png"
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                        alt="Shield Logo"
+                    />
+                </Navbar.Brand>
+                <Navbar.Brand
+                    href="/"
+                    className="font-weight-bold text-uppercase"
+                    style={{ color: "white" }}
                 >
-                  Profile
-                </Nav.Link>
-                <Nav.Link
-                  href="/emergency"
-                  className="mx-3"
-                  style={{ color: "#D73CBE" }}
-                >
-                  Emergency
-                </Nav.Link>
-              </>
-            ) : (
-              <>
-                <Button
-                  onClick={() => navigate("/auth/signin")}
-                  variant="outline-dark"
-                  className="mx-3"
-                >
-                  Sign In
-                </Button>
-                <Button
-                  onClick={() => navigate("/auth/signup")}
-                  variant="outline-dark"
-                  className="mx-3"
-                >
-                  Sign Up
-                </Button>
-              </>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+                    Shield
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link
+                            href="/about"
+                            className="mx-3"
+                            style={{ color: "#D73CBE" }}
+                        >
+                            About
+                        </Nav.Link>
+                        <Nav.Link
+                            href="/services"
+                            className="mx-3"
+                            style={{ color: "#D73CBE" }}
+                        >
+                            Services
+                        </Nav.Link>
+                        <Nav.Link
+                            href="/community"
+                            className="mx-3"
+                            style={{ color: "#D73CBE" }}
+                        >
+                            Community
+                        </Nav.Link>
+                        <Nav.Link
+                            href="/contact"
+                            className="mx-3"
+                            style={{ color: "#D73CBE" }}
+                        >
+                            Contact
+                        </Nav.Link>
+                        {user !== null ? (
+                            <>
+                                <Nav.Link
+                                    href="/profile"
+                                    className="mx-3"
+                                    style={{ color: "#D73CBE" }}
+                                >
+                                    Profile
+                                </Nav.Link>
+                                <Nav.Link
+                                    href="/emergency"
+                                    className="mx-3"
+                                    style={{ color: "#D73CBE" }}
+                                >
+                                    Emergency
+                                </Nav.Link>
+                            </>
+                        ) : (
+                            <>
+                                <Button
+                                    onClick={() => navigate("/auth/signin")}
+                                    variant="outline-dark"
+                                    className="mx-3"
+                                >
+                                    Sign In
+                                </Button>
+                                <Button
+                                    onClick={() => navigate("/auth/signup")}
+                                    variant="outline-dark"
+                                    className="mx-3"
+                                >
+                                    Sign Up
+                                </Button>
+                            </>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
 export default NavigationBar;
--- client/src/components/Signin.jsx
+++ client/src/components/Signin.jsx
@@ -47,10 +47,10 @@ const Signin = ({ setUser }) => {
   return (
     <section className="h-100">
       <div className="container h-100">
-        <div className="row justify-content-sm-center h-100">
+        <div className="row justify-content-sm-center h-100 mt-5">
           <div className="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
             <div className="card shadow-lg">
-              <div className="card-body p-5">
+              <div className="card-body p-4 mt-3">
                 <h1 className="fs-4 card-title fw-bold mb-4">Login</h1>
                 <form
                   onSubmit={formik.handleSubmit}
